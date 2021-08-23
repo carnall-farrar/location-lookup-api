6847f0deacb5a5730dca137769c7698cf3f665ec
jmjpickard <jack.pickard@carnallfarrar.com>
2021-08-23 16:07:14 -0400

    notebook now loads to heroku db

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,4 @@
+from .config import Config, ProductionConfig, set_up_logging
 from flask import Flask
 from flask.helpers import get_root_path
 from flask_migrate import Migrate
@@ -11,7 +12,6 @@ import logging
 db = SQLAlchemy()
 migrate = Migrate()
 
-from .config import Config, ProductionConfig, set_up_logging
 
 set_up_logging()
 logger = logging.getLogger(__name__)
@@ -22,17 +22,17 @@ def create_app():
     server = Flask(__name__)
     if environ.get("ENV") == 'development':
         server.config.from_object(Config)
-        logging.log(logging.INFO,server.config['SQLALCHEMY_DATABASE_URI'])
+        logging.log(logging.INFO, server.config['SQLALCHEMY_DATABASE_URI'])
     if environ.get("ENV") == 'production':
         server.config.from_object(ProductionConfig)
 
     # Meta tags for viewport responsiveness
     register_extensions(server)
-    
+
     @server.route('/', methods=['GET', 'POST'])
     def hello():
         return 'Hello world!'
-    
+
     return server
 
 
@@ -42,9 +42,8 @@ def register_extensions(server):
     migrate.init_app(server, db)
 
 
-
-
 if __name__ == '__main__':
-    ROOT_DIR = os.path.dirname(os.path.dirname(__file__))  # assuming this file in <proj_root>/app
+    # assuming this file in <proj_root>/app
+    ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
     OUTPUTS = os.path.join(ROOT_DIR, 'outputs')
     print(OUTPUTS)
