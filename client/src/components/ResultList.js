7d7bff8a3d8ec5da9002b7d881c8f3b41ee794be
Leo Edwards <leo.edwards@carnallfarrar.com>
2021-09-01 13:23:58 +0100

    make the result list pretty

--- client/src/components/ResultList.js
+++ client/src/components/ResultList.js
@@ -1,22 +1,44 @@
 import React from 'react';
 
 import { sanitize } from 'dompurify';
-import { Card } from 'react-bootstrap';
+import { Card, Table } from 'react-bootstrap';
 
 function ResultList ({ results }) {
   const resultItems = results.map(result =>
     <Card className='mb-3' key={result.id}>
       <Card.Body>
-        <Card.Title
-          dangerouslySetInnerHTML={{
-            __html: `${sanitize(result.ccg_name)} ${sanitize(result.stp_name)}`
-          }}
-        ></Card.Title>
-        <Card.Subtitle
-          className='mb-2 text-muted'
-        >{result.ccg_code} | {result.stp_code} | {result.region_code}
+        <Card.Title>{result.ccg_name}</Card.Title>
+        <Card.Subtitle className='mb-2 text-muted'>
+          CCG Code: {result.ccg_code} | STP Code: {result.stp_code} | Region Code: {result.region_code}
         </Card.Subtitle>
-        <Card.Text dangerouslySetInnerHTML={{ __html: sanitize(result.ccg_name) }} />
+        <Card.Text>
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>Code</th>
+                <th>Name</th>
+                <th>CDH</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{result.ccg_code}</td>
+                <td>{result.ccg_name}</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>{result.stp_code}</td>
+                <td>{result.stp_name}</td>
+                <td>{result.stp_cdh}</td>
+              </tr>
+              <tr>
+                <td>{result.region_code}</td>
+                <td>{result.region_name}</td>
+                <td>{result.region_cdh}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
