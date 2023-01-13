28858dd8927e3410847957262247867ab88a9417
rami <ramireddykowaluru@gmail.com>
2023-01-13 13:02:11 +0000

    fixing code issues

--- server/app/api/location/models.py
+++ server/app/api/location/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy.dialects.postgresql import TSVECTOR
+import logging as logger
 
 from app import db
 
@@ -64,45 +65,52 @@ class Words(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(200))
 
-
-function_snippet = db.DDL("""
-    CREATE FUNCTION tsv_searchable_text_trigger() RETURNS trigger AS $$
-    begin
-    new.tsv_searchable_text :=
-        setweight(to_tsvector('pg_catalog.english', coalesce(new.ccg_code,'')), 'B') ||
-        setweight(to_tsvector('pg_catalog.english', coalesce(new.ccg_name,'')), 'A') ||
-        setweight(to_tsvector('pg_catalog.english', coalesce(new.stp_code,'')), 'C') ||
-        setweight(to_tsvector('pg_catalog.english', coalesce(new.stp_cdh,'')), 'B') ||
-        setweight(to_tsvector('pg_catalog.english', coalesce(new.stp_name,'')), 'A') ||
-        setweight(to_tsvector('pg_catalog.english', coalesce(new.region_code,'')), 'C') ||
-        setweight(to_tsvector('pg_catalog.english', coalesce(new.region_cdh,'')), 'B') ||
-        setweight(to_tsvector('pg_catalog.english', coalesce(new.region_name,'')), 'A');
-    return new;
-    end
-    $$ LANGUAGE plpgsql;
-""")
-
-trigger_snippet = db.DDL("""
-    CREATE TRIGGER searchable_text_update BEFORE INSERT OR UPDATE
-    ON ccg_lookup
-    FOR EACH ROW EXECUTE PROCEDURE tsv_searchable_text_trigger();
-""")
-
-add_words_snippet = db.DDL("""
-        INSERT INTO words (word)
-        SELECT word FROM ts_stat('
-          SELECT to_tsvector(''simple'', ccg_code) ||
-                 to_tsvector(''simple'', coalesce(ccg_name, '''')) ||
-                 to_tsvector(''simple'', stp_code) ||
-                 to_tsvector(''simple'', stp_cdh) ||
-                 to_tsvector(''simple'', coalesce(stp_name, '''')) ||
-                 to_tsvector(''simple'', region_code) ||
-                 to_tsvector(''simple'', region_cdh) ||
-                 to_tsvector(''simple'', coalesce(region_name, ''''))
-            FROM ccg_lookup
-        ');
+try:
+    function_snippet = db.DDL("""
+        CREATE FUNCTION tsv_searchable_text_trigger() RETURNS trigger AS $$
+        begin
+        new.tsv_searchable_text :=
+            setweight(to_tsvector('pg_catalog.english', coalesce(new.ccg_code,'')), 'B') ||
+            setweight(to_tsvector('pg_catalog.english', coalesce(new.ccg_name,'')), 'A') ||
+            setweight(to_tsvector('pg_catalog.english', coalesce(new.stp_code,'')), 'C') ||
+            setweight(to_tsvector('pg_catalog.english', coalesce(new.stp_cdh,'')), 'B') ||
+            setweight(to_tsvector('pg_catalog.english', coalesce(new.stp_name,'')), 'A') ||
+            setweight(to_tsvector('pg_catalog.english', coalesce(new.region_code,'')), 'C') ||
+            setweight(to_tsvector('pg_catalog.english', coalesce(new.region_cdh,'')), 'B') ||
+            setweight(to_tsvector('pg_catalog.english', coalesce(new.region_name,'')), 'A');
+        return new;
+        end
+        $$ LANGUAGE plpgsql;
     """)
+except Exception as e:
+    logger.info(e)
 
+try:
+    trigger_snippet = db.DDL("""
+        CREATE TRIGGER searchable_text_update BEFORE INSERT OR UPDATE
+        ON ccg_lookup
+        FOR EACH ROW EXECUTE PROCEDURE tsv_searchable_text_trigger();
+    """)
+except Exception as e:
+    logger.info(e)
+
+try:
+    add_words_snippet = db.DDL("""
+            INSERT INTO words (word)
+            SELECT word FROM ts_stat('
+            SELECT to_tsvector(''simple'', ccg_code) ||
+                    to_tsvector(''simple'', coalesce(ccg_name, '''')) ||
+                    to_tsvector(''simple'', stp_code) ||
+                    to_tsvector(''simple'', stp_cdh) ||
+                    to_tsvector(''simple'', coalesce(stp_name, '''')) ||
+                    to_tsvector(''simple'', region_code) ||
+                    to_tsvector(''simple'', region_cdh) ||
+                    to_tsvector(''simple'', coalesce(region_name, ''''))
+                FROM ccg_lookup
+            ');
+        """)
+except Exception as e:
+    logger.info(e)
 
 db.event.listen(CcgToStpLookup.__table__, 'after_create', function_snippet.execute_if(dialect='postgresql'))
 db.event.listen(CcgToStpLookup.__table__, 'after_create', trigger_snippet.execute_if(dialect='postgresql'))
